7c4d7876d8e3240c7baed644477164ab9d6da04f
oftalmoflick <oftalmoflick@gmail.com>
2021-04-10 18:46:51 -0500

    Se le aumento analisis de regresion lineal y modelo de regresion generalizado con distribucion poisson

--- Mortalidad_hospitalaria.R
+++ Mortalidad_hospitalaria.R
@@ -1,4 +1,6 @@
 library(psych)
+library(graphics)
+library(stats)
 #Existe diferencia en incidencia de mortalidad
 # en diferentes UCI
 
@@ -35,16 +37,47 @@ Mortalidad_hospitalaria_apache_4a <- merge(x =  Mortalidad_hospitalaria, y = Apa
 # mientras que otros veian dos pacientes, uno fallecia y ya tenian mortalidad del 50%
 Mortalidad_hospitalaria_apache_4a <-filter(Mortalidad_hospitalaria_apache_4a, Sobrevivió >= 25)
 
-
-#xiste correlacion entre la probabilidad de fallcer y el reisgo de fallecer medido por APACHE 4a en los diferentes hospitales
-  # si existe esta correlacion y es de un r= 68
-pairs.panels(Mortalidad_hospitalaria_apache_4a)
-
-plot(Mortalidad_hospitalaria_apache_4a$porcentaje_mortalidad, Mortalidad_hospitalaria_apache_4a$Mediana_Apache_4a)
-
 #Se crea grafico de densidad para encontrar la Bimodalidad en la curva
 # Algunas UCI tienen mortalidad de un 6.5% 
 # mientras que otras tienen mortalidad del 11.5%
 ggplot(Mortalidad_hospitalaria_apache_4a)+ 
   aes(porcentaje_mortalidad)+
   geom_density()
+
+# La mediana de Apache 4a tiene una distribicion tambien binomal
+ggplot(Mortalidad_hospitalaria_apache_4a)+ 
+  aes(Mediana_Apache_4a)+
+  geom_density()
+
+#xiste correlacion entre la probabilidad de fallcer y el reisgo de fallecer medido por APACHE 4a en los diferentes hospitales
+  # si existe esta correlacion y es de un r= 68
+pairs.panels(Mortalidad_hospitalaria_apache_4a)
+
+#analisis de regresion lineal simple
+lm_mortalidad_apachea4 <- lm(data = Mortalidad_hospitalaria_apache_4a, formula = porcentaje_mortalidad ~ Mediana_Apache_4a)
+summary(lm_mortalidad_apachea4)
+
+#grafico de regresion lineal simple
+plot(Mortalidad_hospitalaria_apache_4a$Mediana_Apache_4a, Mortalidad_hospitalaria_apache_4a$porcentaje_mortalidad)
+abline(lm_mortalidad_apachea4, lwd = 3, col = "red")
+
+
+#graficos para analizar residuos
+par(mfrow = c(2,2))
+plot(lm_mortalidad_apachea4)
+
+ar(mfrow = c(1,1))
+
+# se realizo modelo de regresion possion y no se encontro buena correlacion
+glm_mortalidad_apachea4 <- glm(data = Mortalidad_hospitalaria_apache_4a, formula = porcentaje_mortalidad ~ Mediana_Apache_4a, family = poisson())
+summary(glm_mortalidad_apachea4)
+
+plot(Mortalidad_hospitalaria_apache_4a$Mediana_Apache_4a, Mortalidad_hospitalaria_apache_4a$porcentaje_mortalidad)
+abline(glm_mortalidad_apachea4, lwd = 3, col = "red")
+
+par(mfrow = c(2,2))
+plot(glm_mortalidad_apachea4)
+
+par(mfrow = c(1,1))
+
+
